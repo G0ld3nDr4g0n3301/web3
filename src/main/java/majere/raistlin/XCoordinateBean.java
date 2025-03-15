b01b6722f4536eb7fa19fe81f2d8630c2d52dacb
G0ld3nDr4g0n3301 <nuchtonibudushe@mail.ru>
2025-03-15 03:23:15 +0300

    Removed lombok and added javadoc target

--- src/main/java/majere/raistlin/XCoordinateBean.java
+++ src/main/java/majere/raistlin/XCoordinateBean.java
@@ -1,8 +1,5 @@
 package majere.raistlin;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 //import jakarta.annotation.*;
 import java.io.Serializable;
@@ -14,8 +11,6 @@ import jakarta.inject.Named;
 
 @Named("xCoordinateBean")
 @SessionScoped
-@Data
-@NoArgsConstructor
 public class XCoordinateBean implements Serializable {
     private Float x = 0f;
 
